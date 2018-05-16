d67a367970b5d196e84d486f04012fc2701abc7b
Mr. S <matthewhanyao@gmail.com>
2018-05-16 11:44:35 -0400

    added receiveMessage struct

--- TankWorldActions.swift
+++ TankWorldActions.swift
@@ -9,5 +9,31 @@
 import Foundation
 
 extension TankWorld {
-    
+    func actionSendMessage(tank: Tank, sendMessageAction: SendMessageAction){
+        if isDead(tank){return}
+        //logger.addLog(tank, "Sending Message \(sendMessageAction")
+
+        if !isEnergyAvailable(tank, amount: Constants.costOfSendingMessage){
+            //logger.addLog(tank, "Insufficient energy to send message")
+            return
+        }
+
+        //applyCost(tank, amount: Constants.costOfSendingMessage)
+        //MessageCenter.sendMessage()
+    }
+
+    func actionReceiveMessage(tank: Tank, receiveMessageAction: ReceiveMessageAction){
+        if isDead(tank){return}
+        //logger.addLog(tank, "Receiving Message \(receiveMessageAction)")
+
+        if !isEnergyAvailable(tank, amount: Constants.costOfReceivingMessage){
+            //logger.addLog(tank, "Insufficient energy to send message")
+            return
+        }
+
+        //applyCost(tank, amount: Constants.costOfReceivingMessage)
+        //let message = MessageCenter.receiveMessage(id: receiveMessageAction.id)
+        //tank.setReceivedMessage(receivedMessage: message)
+
+    }
 }
--- TankWorldSupport.swift
+++ TankWorldSupport.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 extension TankWorld {
+    func applyCost (tank: Tank, amount: Int){
+        tank.useEnergy(amount: amount)
+    }
+
+
     func newPosition (position: Position, direction: Direction, magnitude k: Int) -> Position {
         let x = position.row
         let y = position.col
@@ -23,32 +28,32 @@ extension TankWorld {
         case .southwest: return Position(x + k, y - k)
         }
     }
-    
+
     func isGoodIndex (_ row: Int, _ col: Int) -> Bool {
         return row <= 14 && col <= 14 && row >= 0 && col >= 0
     }
-    
+
     func isValidPosition (_ position: Position) -> Bool {
         return isGoodIndex(position.row, position.col)
     }
-    
+
     func distance (_ p1: Position, _ p2: Position) -> Int {
         let roughResult = Double((p1.row - p2.row) * (p1.row - p2.row) + (p1.col - p2.col) * (p1.col - p2.col))
         return Int(sqrt(roughResult))
     }
-    
+
     func isDead (_ gameObject: GameObject) -> Bool {
         return gameObject.energy < 0
     }
-    
+
     func isEnergyAvailable (_ gameObject: GameObject, amount: Int) -> Bool {
         return gameObject.energy > amount
     }
-    
+
     func isPositionEmpty (_ position: Position) -> Bool {
         return grid[position.row][position.col] == nil
     }
-    
+
     func findAllGameObjects () -> [GameObject] {
         var result = [GameObject]()
         for e in grid {
@@ -60,7 +65,7 @@ extension TankWorld {
         }
         return result
     }
-    
+
     func getLegalSurroundingPositions (_ position: Position) -> [Position] {
         var result = [Position]()
         result.append(newPosition(position: position, direction: .north, magnitude: 1))
@@ -73,7 +78,7 @@ extension TankWorld {
         result.append(newPosition(position: position, direction: .southeast, magnitude: 1))
         return result.filter{isValidPosition($0)}
     }
-    
+
     func findObjectsWithinRange (_ position: Position, range: Int) -> [Position] {
         var result = [Position]()
         for e in 0...14 {
@@ -85,22 +90,22 @@ extension TankWorld {
         }
         return result
     }
-    
+
     func findAllTanks () -> [Tank] {
         return findAllGameObjects().filter{$0.objectType == .Tank} as! [Tank]
     }
-    
+
     func findAllRovers () -> [Mine] {
         return findAllGameObjects().filter{$0.objectType == .Rover} as! [Mine]
     }
-    
+
     func findWinner () -> Tank? {
         if findAllTanks().count == 1 {
             return findAllTanks()[0]
         }
         return nil
     }
-    
+
     func randomizeDirection () -> Direction {
         var x = Set<Direction>()
         x.insert(.north)
@@ -115,7 +120,7 @@ extension TankWorld {
         let y = Array(x)
         return y[0]
     }
-    
-    
-    
+
+
+
 }
