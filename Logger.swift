158bb6f56756836d9148622ec5926bb09f86e37a
Mr. S <matthewhanyao@gmail.com>
2018-05-21 13:59:12 -0400

    added logger struct

--- Logger.swift
+++ Logger.swift
@@ -1,2 +1,18 @@
 import Foundation
 import Glibc
+
+struct Logger{
+    var data: [Int : [String]]
+    var round = 1
+
+    func addLog(_ tank: Tank, _ message: String){
+        var log = ""
+        log += tank.id + " "
+        log += tank.position.description + " "
+        log += message
+    }
+
+    mutating func newRound(){
+        round += 1
+    }
+}
